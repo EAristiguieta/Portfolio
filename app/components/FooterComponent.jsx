26be241917164fe57ea47f577b54680948c0b0b2
edson <edsonaristiguieta@gmail.com>
2024-02-20 12:41:29 +0100

    Logos redes sociales

--- app/components/FooterComponent.jsx
+++ app/components/FooterComponent.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 export const FooterComponent = () => {
@@ -5,7 +6,28 @@ export const FooterComponent = () => {
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
       <div className="container p-12 flex justify-between">
         {/*TODO logo de redes */}
-        <span>Comming next 😁</span>
+        <Link
+          href="https://www.linkedin.com/in/edson-aristiguieta-vidal/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src="/images/icons/linkedin.svg"
+            alt="LinkedIn"
+            className="w-6 h-6"
+          />
+        </Link>
+        <Link
+          href="https://github.com/EAristiguieta"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src="/images/icons/github.svg"
+            alt="GitHub"
+            className="w-6 h-6"
+          />
+        </Link>
         <p className="text-slate-600">All rights reserved.</p>
       </div>
     </footer>
