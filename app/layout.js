65e992b945904975b4aa8f55c1ef0fc01cf8d68a
edson <edsonaristiguieta@gmail.com>
2024-02-20 18:31:47 +0100

    Arreglo favicon

--- app/layout.js
+++ app/layout.js
@@ -6,6 +6,11 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Porfolio",
   description: "V1",
+  icons: {
+    icon: ["/favicon.ico?=v4"],
+    apple: ["/apple-touch-icon.png?v=4"],
+    shortcut: ["/apple-touch-icon.png"],
+  },
 };
 
 export default function RootLayout({ children }) {
