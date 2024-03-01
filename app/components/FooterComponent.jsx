b154aed27848a0199aac68f6e3123357f1cfdf52
edson <edsonaristiguieta@gmail.com>
2024-03-01 20:24:23 +0100

    Contacto Añadido

--- app/components/FooterComponent.jsx
+++ app/components/FooterComponent.jsx
@@ -1,7 +1,26 @@
+"use client";
 import Link from "next/link";
 import React from "react";
+import { useEffect, useState } from "react";
 
 export const FooterComponent = () => {
+  const email = "edsonaristiguieta@gmail.com";
+  const [clicked, setClicked] = useState(false);
+
+  useEffect(() => {
+    const copyToClipboard = () => {
+      navigator.clipboard.writeText(email);
+      alert(" Correo copiado al portapapeles. ¡Gracias por conectar!");
+    };
+
+    const button = document.getElementById("copyButton");
+    button.addEventListener("click", copyToClipboard);
+
+    return () => {
+      button.removeEventListener("click", copyToClipboard);
+    };
+  }, []);
+
   return (
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
       <div className="container p-12 flex justify-evenly">
@@ -28,6 +47,13 @@ export const FooterComponent = () => {
             className="w-10 h-10"
           />
         </Link>
+        <button id="copyButton" onClick={() => setClicked(true)}>
+          <img
+            src="/images/icons/send.svg"
+            alt="GitHub"
+            className="w-10 h-10"
+          />
+        </button>
       </div>
     </footer>
   );
